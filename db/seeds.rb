3c97fb6f02179626a8f86c3c6df5d01b3af3e14d
chornamarta <marta.chornaa@gmail.com>
2022-08-21 00:18:18 +0300

    create user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:"Ivab",password:"password")
+User.create(username:"petro",password:"password")
+User.create(username:"anna",password:"password")
+User.create(username:"alin",password:"password")
+User.create(username:"kebaba",password:"password")
