0c7b1d2700511d37968f42f720eda7991c47b6c7
chornamarta <marta.chornaa@gmail.com>
2022-08-25 18:24:58 +0300

    login backend

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,23 @@
 class SessionController < ApplicationController
-    def new 
-        
+  def new 
+
+  end
+
+  def create 
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id 
+      flash[:success] = "Success"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Something is wrong with your information"
+      render 'new'
     end
+  end
+
+  def destroy
+    session[:user_id] = nil 
+    flash[:success] = "Logout success"
+    redirect_to login_path
+  end
 end
