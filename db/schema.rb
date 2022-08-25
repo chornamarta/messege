bb1f69b7994f2b323040d64a9f2ee374d19c9342
chornamarta <marta.chornaa@gmail.com>
2022-08-25 17:32:58 +0300

    create message model and show it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_20_210444) do
+ActiveRecord::Schema.define(version: 2022_08_25_135824) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
